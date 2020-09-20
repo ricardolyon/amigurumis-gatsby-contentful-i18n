67fb5391cfb0143799574b0e46e9a90b82943af0
Ricardo Enrique Lyon <ricardolyon@gmail.com>
2020-09-19 20:39:45 -0500

    PRUEB-12 | Product Catalog View

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,26 @@ import React from "react"
 import { Link, graphql } from 'gatsby'
 
 import Layout from "../components/layout"
-import Img from 'gatsby-image'
 import SEO from "../components/seo"
+import "./index.css"
 
 const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
     <h1>Dolls</h1>
     {props.data.allContentfulDoll.nodes.map((el, idx) => {
-      const { name, childContentfulDollDescriptionRichTextNode, pictures } = el;
+      const { name, childContentfulDollDescriptionRichTextNode, pictures, size, price } = el;
       if (name && pictures) {
-        return <div className="card" key={idx}>
-          <Link to={name}>{name}</Link>
-          <div>{childContentfulDollDescriptionRichTextNode.content[0].content[0].value}</div>
-          <img src={pictures[0].fluid.srcWebp} />
+        return <div className="card am-doll" key={idx}>
+          <h2><Link to={name}>{name}</Link><span className="small"> - Size: {size}</span></h2>
+          <div className="flex">
+            <div className="description">{childContentfulDollDescriptionRichTextNode.content[0].content[0].value}</div>
+            <img className="picture" src={pictures[0].fluid.srcWebp} />
+          </div>
+          <p>Price: {price}</p>
         </div>
       } else {
-        return <div></div>
+        return
       }
     })}
   </Layout>
@@ -39,6 +42,7 @@ query {
           }
         }
       }
+      price
       color
       material
       size
